04c3dba1846df32687b1eff4fa445a4e20205ba3
azizoid <shaqhuseynov@gmail.com>
2024-08-08 12:57:44 +0200

    feat: include modal on the App

--- src/App.tsx
+++ src/App.tsx
@@ -2,20 +2,22 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import { useState } from 'react';
+import { Modal } from './components/Modal/Modal';
 
 export const App = () => {
-  const [isModalOpen, setModalOpen] = useState<boolean>(false);
+  const [isModalOpen, setModalOpen] = useState(false);
 
   const handleOpenModal = () => {
-
+    setModalOpen(true);
   };
 
   const handleCloseModal = () => {
-
+    setModalOpen(false);
   };
 
   const handleSave = () => {
-
+    console.log('Save clicked');
+    setModalOpen(false);
   };
 
   return (
@@ -30,7 +32,10 @@ export const App = () => {
       </div>
       <h1>Vite + React</h1>
       <div className="card">
-
+        <button onClick={handleOpenModal}>Open Modal</button>
+        <Modal show={isModalOpen} onClose={handleCloseModal} onSave={handleSave} title="Modal Title">
+          <p>This is the modal content</p>
+        </Modal>
       </div>
     </>
   )
